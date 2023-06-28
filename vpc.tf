6fad0eebeffdbe59e135991a9f2d3d41d7f81894
jasonschimdt <137985196+jasonschimdt@users.noreply.github.com>
2023-06-28 12:14:38 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 //  Define the VPC.
 resource "aws_vpc" "openshift" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = "${}"
   enable_dns_hostnames = true
   enable_dns_support   = true
   instance_tenancy     = "default"
